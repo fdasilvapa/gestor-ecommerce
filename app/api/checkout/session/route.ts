2e7c9872c02cec5f4a18f00bfe1a9598eaa608ca
Felipe <felipedasilva23785@gmail.com>
2025-11-17 09:39:46 -0300

    fix(cart): Corrige implementação de pagamentos e carrinho

--- app/api/checkout/session/route.ts
+++ app/api/checkout/session/route.ts
@@ -1,50 +1,48 @@
 import { NextResponse } from "next/server";
 import { CartItem } from "@/app/types";
-
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-10-29.clover",
-});
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("A variável de ambiente STRIPE_SECRET_KEY está faltando.");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  typescript: true,
+} as any);
 
 export async function POST(request: Request) {
   try {
     const cartItems = (await request.json()) as CartItem[];
 
+    const origin = request.headers.get("origin") || "http://localhost:3001";
+
     if (!cartItems || cartItems.length === 0) {
-      return NextResponse.json(
-        { message: "O carrinho está vazio." },
-        { status: 400 }
-      );
+      return NextResponse.json({ message: "Carrinho vazio." }, { status: 400 });
     }
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
-      cartItems.map((item) => ({
-        price_data: {
-          currency: "brl",
-          product_data: {
-            name: item.name,
-          },
-
-          unit_amount: Math.round(parseFloat(item.price) * 100),
+    const line_items = cartItems.map((item) => ({
+      price_data: {
+        currency: "brl",
+        product_data: {
+          name: item.name,
         },
-        quantity: item.quantity,
-      }));
-
-    const success_url = `http:localhost:3001/sucesso?session_id={CHECKOUT_SESSION_ID}`;
-    const cancel_url = `http:localhost:3001/falha`;
+        unit_amount: Math.round(parseFloat(item.price) * 100),
+      },
+      quantity: item.quantity,
+    }));
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       line_items: line_items,
-      success_url: success_url,
-      cancel_url: cancel_url,
+      success_url: `${origin}/sucesso?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/falha`,
     });
 
-    return NextResponse.json({ sessionId: session.id });
+    return NextResponse.json({ url: session.url });
   } catch (error) {
-    console.error("Erro ao criar sessão do Stripe:", error);
+    console.error("Erro no Backend do Stripe:", error);
+
     if (error instanceof Error) {
       return NextResponse.json({ message: error.message }, { status: 500 });
     }
